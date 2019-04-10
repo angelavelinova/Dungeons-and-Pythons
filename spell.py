fc965abc4d33f97cfe37ce86ae8bd43a06b5e299
Angela Velinova <angela.velinova@abv.bg>
2019-04-10 17:48:46 +0300

    Add class spell

---
+++ spell.py
@@ -0,0 +1,8 @@
+class Spell:
+	def __init__(self, name, damage, mana_cost, cast_range):
+		self.name = name
+		self.damage = damage
+		self.mana_cost = mana_cost
+		self.cast_range = cast_range
+
+harmless_spell = Spell("harmless spell", 0, 0, 1)
